35725ffbd86487e218a5b590c225f4bf22109097
EdiRestrepo <edisonestival@gmail.com>
2023-05-26 23:44:40 -0500

    Finished Homework #1-react

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import './FriendList.css'
+import css from './FriendList.module.css'
 
 const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css["friend-list"]}>
       {friends.map(({avatar, name, isOnline, id}) => {
         return (
-          <li key={id} className="item">
-            <span className={isOnline ? 'status' : '' } ></span>
-            <img className="avatar" src={avatar} alt="User avatar" width="48" />
-            <p className="name">{name}</p>
+          <li key={id} className={css["item"]}>
+            <span className={isOnline ? css["available"] : css["not-available"] } ></span>
+            <img className={css["avatar"]} src={avatar} alt="User avatar" width="48" />
+            <p className={css["name"]}>{name}</p>
           </li>
         );
       })}
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import './TransactionHistory.css';
+import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({items}) => {
+
+  function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+    <table className={css["transaction-history"]}>
+      <thead className={css["head"]}>
+        <tr className={css['head-table']}>
+          <th className={css["title-table"]}>TYPE</th>
+          <th className={css["title-table"]}>AMOUNT</th>
+          <th className={css["title-table"]}>CURRENCY</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(({id, type, amount, currency}) => {
           return (
-            <tr key={id}>
-              <td>{type}</td>
+            <tr key={id} className={css["arrow-table"]}>
+              <td>{capitalizeFirstLetter(type)}</td>
               <td>{amount}</td>
               <td>{currency}</td>
             </tr>
